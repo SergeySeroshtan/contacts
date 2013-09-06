daa3e43fafee919f6b356e9d0812fdb6705d2539
Antony Grytsenko <anthony.grytsenko@gmail.com>
2013-09-06 14:34:10 +0300

    Added comments.

--- modules/contacts-api/src/main/java/grytsenko/contacts/api/Contact.java
+++ modules/contacts-api/src/main/java/grytsenko/contacts/api/Contact.java
@@ -26,66 +26,117 @@ public final class Contact implements Serializable {
     public Contact() {
     }
 
+    /**
+     * Returns the unique name, which can be used to distinguish contacts.
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Sets the unique name.
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Returns the first name.
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Sets the first name.
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * Returns the last name.
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Sets the last name.
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * Returns the URL of photo.
+     */
     public String getPhotoUrl() {
         return photoUrl;
     }
 
+    /**
+     * Sets the URL of photo.
+     */
     public void setPhotoUrl(String photoUrl) {
         this.photoUrl = photoUrl;
     }
 
+    /**
+     * Returns the work email address.
+     */
     public String getMail() {
         return mail;
     }
 
+    /**
+     * Sets the work email address.
+     */
     public void setMail(String mail) {
         this.mail = mail;
     }
 
+    /**
+     * Returns the mobile phone number.
+     */
     public String getPhone() {
         return phone;
     }
 
+    /**
+     * Sets the mobile phone number.
+     */
     public void setPhone(String phone) {
         this.phone = phone;
     }
 
+    /**
+     * Returns the location of office.
+     */
     public String getLocation() {
         return location;
     }
 
+    /**
+     * Sets the location of office.
+     */
     public void setLocation(String location) {
         this.location = location;
     }
 
+    /**
+     * Returns the version.
+     * 
+     * <p>
+     * Versions can be compared for equality only.
+     */
     public String getVersion() {
         return version;
     }
 
+    /**
+     * Sets the version.
+     */
     public void setVersion(String version) {
         this.version = version;
     }
